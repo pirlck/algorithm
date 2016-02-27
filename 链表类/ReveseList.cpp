6a914b7774e193d6419cfd81d4d0dce3660ed718
pirlck <kuangcailiang07@gmail.com>
2016-02-27 14:35:42 +0800

    swap 2 pointers addr!

--- 链表类/ReveseList.cpp
+++ 链表类/ReveseList.cpp
@@ -11,22 +11,70 @@ struct Node{
 	Node* next;
 };
 
-//单链表的转置,循环方法
-Node* reverseByLoop(Node *head)
+Node* InitList( )
 {
-	if (head == NULL || head->next == NULL)
-		return head;
-	Node *pre = NULL;
-	Node *next = NULL;
-	while (head != NULL)
-	{
-		next = head->next;
+	Node* head = new Node;
+	head->data = 0;
+	head->next = NULL;
+	return head;
+}
 
-		head->next = pre;
-		pre = head;
-		head = next;
+int InsertNode(Node** head,int data)
+{
+	Node* inode = new Node;
+	inode->data = data;
+	
+	inode->next = *head;
+	*head = inode;
+	return 0;
+}
+
+int DesList(Node* head)
+{
+	Node* pTmp;
+	while (head)
+	{
+		pTmp = head->next;
+		delete head;
+		head = pTmp;
 	}
-	return pre;
+	return 0;
+}
+
+void PrintList(Node* head)
+{
+	Node* tmp_node = head;
+	while (tmp_node)
+	{
+		cout << tmp_node->data << "->";
+		tmp_node = tmp_node->next;
+	}
+	cout << endl;
+}
+
+
+
+//单链表的转置,循环方法
+//单层指针自己本身的值并没有改变
+//用双层指针传入指针地址，然后就可以改变
+//此处仅为复习下(int* a,int* b)知识点
+//指针的拷贝，改变需从地址开始
+void* ReverList(Node** head)
+{
+	if ( (*head) == NULL || (*head)->next == NULL)
+		return NULL;
+	Node* pre = NULL;
+	Node* next = NULL;
+
+	while (*head != NULL)
+	{
+		next = (*head)->next;
+		(*head)->next = pre;
+		pre = (*head);
+		*head = next;
+	}
+	*head = pre;
+	return NULL;
 }
 
 //单链表的转置,递归方法
@@ -45,10 +93,20 @@ Node* reverseByRecursion(Node *head)
 }
 
 
-//两种写法，还没来得及测试
 int main()
 {
+	Node* pList = InitList();
+	
+	for (int i=1 ; i < 20; i++)
+	{
+		InsertNode(&pList, i);
+	}
+	PrintList(pList);
+	
+    ReverList(&pList);
+	PrintList(pList);
 
+	DesList(pList);
+	return 0;
 }
-
 
